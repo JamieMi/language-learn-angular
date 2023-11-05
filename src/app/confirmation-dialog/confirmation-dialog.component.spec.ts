12e74448755697058385aa086bbf4d68312988a5
jamie.mitchinson <jamie.nmitchinson@gmail.com>
2023-11-05 17:23:39 +0000

    component harness

--- src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,7 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ConfirmationDialogComponent } from './confirmation-dialog.component';
 
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed'; //  contains symbols that are used only in Karma tests.
+import {MatButtonHarness} from '@angular/material/button/testing';
+let loader: HarnessLoader;
+let fixture: any;
+
+describe('ConfirmationDialogComponentWithComponentHarness', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({/*imports: [MyModule], *//*declarations: [ConfirmationDialogComponent]*/})
+        .compileComponents();
+    fixture = TestBed.createComponent(ConfirmationDialogComponent);
+    let loader = TestbedHarnessEnvironment.loader(fixture);
+  });
+
+  it('should mark confirmed when ok button clicked', async () => {
+    const okButton = await loader.getHarness(MatButtonHarness); // === buttons[0]
+    //const okButton = await loader.getHarness(MatButtonHarness.with({selector: '.confirm'});
+    expect(fixture.componentInstance.confirmed).toBe(false);
+    expect(await okButton.isDisabled()).toBe(false);
+    await okButton.click();
+    expect(fixture.componentInstance.confirmed).toBe(true);
+  });
+}
+
+/*
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
@@ -18,4 +42,4 @@ describe('ConfirmationDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+}*/);
